0afa6f53e3a33e6bbd7d6aa09d767b3c78f0abac
Lucas Martinez <lsmartinez88@hotmail.com>
2021-07-03 20:57:34 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ecomFront.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
